8201820acaf7a33ba6307d3db4f61c60f04a43bc
max99xam <pepetrov123@list.ru>
2022-07-04 19:24:10 +0300

    added rewriter middleware

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('data/db.json')
-
 const middlewares = jsonServer.defaults()
 
+
+
+
+const routesRewriter = {
+    "/api/questions/javascript": "/javascriptQuestions",
+    "/api/questions/typescript": "/typescriptQuestions",
+    "/api/questions/nodejs": "/nodejsQuestions",
+}
+
+
+// Middlewares.
 server.use(middlewares)
+server.use(jsonServer.rewriter(routesRewriter));
 server.use(router)
+server.use('/api', router)
 
 const port = process.env.PORT || 3000
 
