693e17c2f56f7fb8a536e322de7594c6214f966a
max99xam <pepetrov123@list.ru>
2022-07-04 19:25:37 +0300

    fixed code style

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('data/db.json')
-const middlewares = jsonServer.defaults()
-
-
-
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('data/db.json');
+const middlewares = jsonServer.defaults();
 
 const routesRewriter = {
-    "/api/questions/javascript": "/javascriptQuestions",
-    "/api/questions/typescript": "/typescriptQuestions",
-    "/api/questions/nodejs": "/nodejsQuestions",
-}
-
+	'/api/questions/javascript': '/javascriptQuestions',
+	'/api/questions/typescript': '/typescriptQuestions',
+	'/api/questions/nodejs': '/nodejsQuestions',
+};
 
 // Middlewares.
-server.use(middlewares)
+server.use(middlewares);
 server.use(jsonServer.rewriter(routesRewriter));
-server.use(router)
-server.use('/api', router)
+server.use(router);
+server.use('/api', router);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-    console.log(`JSON Server is running on port ${port}`)
-})
+	console.log(`JSON Server is running on port ${port}`);
+});
