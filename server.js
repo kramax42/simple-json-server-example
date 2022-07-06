787a18d6b86c70891dabd9240465e6a375ecee3d
max99xam <pepetrov123@list.ru>
2022-07-06 23:50:38 +0300

    added questions size get route

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const server = jsonServer.create();
 const router = jsonServer.router('data/db.json');
 const middlewares = jsonServer.defaults();
 
+
 const routesRewriter = {
 	'/api/questions/javascript': '/javascriptQuestions',
 	'/api/questions/typescript': '/typescriptQuestions',
@@ -12,6 +13,20 @@ const routesRewriter = {
 // Middlewares.
 server.use(middlewares);
 server.use(jsonServer.rewriter(routesRewriter));
+
+
+// Routes.
+server.get('/echo', (req, res) => {
+  res.jsonp(req.query)
+})
+
+// Get questions size.
+server.get(`/api/questions/:technology/size`, (req, res) => {
+  const questionsSize = router.db['__wrapped__'][`${req.params.technology}Questions`].length;
+  res.send({[`${req.params.technology}QuestionsSize`]: questionsSize});
+})
+
+
 server.use(router);
 server.use('/api', router);
 
